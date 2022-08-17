96fed005a56ca63a407879329c6534d86baf62e1
Brandon Duff <brandonduff17@gmail.com>
2022-08-17 19:26:33 -0400

    Chapter two writing out to canvas

--- src/ray_tracer_challenge_clj/tuple.clj
+++ src/ray_tracer_challenge_clj/tuple.clj
@@ -1,5 +1,10 @@
 (ns ray-tracer-challenge-clj.tuple
-  (:require [clojure.math :as math]))
+  (:require
+   [clojure.math :as math]
+   [clojure.string :as str]))
+
+(defn multi-line-string [& strings]
+  (str/join "\n" strings))
 
 (def sum #(reduce + %))
 
@@ -62,3 +67,82 @@
    (- (* (:y t1) (:z t2)) (* (:z t1) (:y t2)))
    (- (* (:z t1) (:x t2)) (* (:x t1) (:z t2)))
    (- (* (:x t1) (:y t2)) (* (:y t1) (:x t2)))))
+
+(defn color [r g b]
+  {:red r :green g :blue b})
+
+(defn hadamard-product [c1 c2]
+  (apply color (map * (vals c1) (vals c2))))
+
+(defn canvas ([width height pixel]
+              (let [rows (into [] (for [i (range height)]
+                                    (into [] (for [j (range width)]
+                                               pixel))))]
+                {:width  width
+                 :height height
+                 :rows   rows}))
+  ([width height]
+   (canvas width height (color 0 0 0))))
+
+(defn write-pixel [canvas x y pixel]
+  (assoc canvas :rows (assoc-in (canvas :rows) [y x] pixel)))
+
+(defn clamp [lower upper n]
+  (cond
+    (< n lower) lower
+    (> n upper) upper
+    :else n))
+
+(defn color->str [color]
+  (->> (vals (multiply-tuple color 255))
+       (map math/round)
+       (map (partial clamp 0 255))
+       (str/join " ")))
+
+(defn pixel-at [canvas x y]
+  (get-in canvas [:rows y x]))
+
+(defn write-line [line]
+  (str/join " " (map color->str line)))
+
+(defn ppm-header [canvas]
+  (multi-line-string "P3"
+                     (str (:width canvas) " " (:height canvas))
+                     "255"))
+
+(defn should-include? [length current-result candidate]
+  (<=
+   (+ (count current-result) (count candidate) 1)
+   length))
+
+(defn merge-word [length lines candidate]
+  (let [last-word (last lines)]
+    (if (should-include? length last-word candidate)
+      (conj (butlast lines) (str last-word " " candidate))
+      (conj lines candidate))))
+
+(defn format-line [length words]
+  (reduce
+     (fn [result current-word]
+       (if (should-include? length (last result) current-word)
+         (conj (vec (butlast result)) (str (last result) " " current-word))
+         (conj result current-word)))
+     [(first words)]
+     (rest words)))
+
+(defn format-body [length string]
+  (apply multi-line-string
+         (reduce (fn [result line]
+             (if (<= (count line) length)
+               (conj result line)
+               (concat result (format-line length (str/split line #"\s")))))
+           []
+           (str/split-lines string))))
+
+(defn canvas-to-ppm [canvas]
+  (let [header (ppm-header canvas)
+        body (->> canvas
+                  :rows
+                  (map write-line)
+                  (apply multi-line-string))]
+    (multi-line-string header (format-body 70 body))))
--- test/ray_tracer_challenge_clj/tuple_test.clj
+++ test/ray_tracer_challenge_clj/tuple_test.clj
@@ -1,6 +1,10 @@
 (ns ray-tracer-challenge-clj.tuple-test
   (:require  [clojure.test :refer :all]
-             [ray-tracer-challenge-clj.tuple :refer :all]))
+             [ray-tracer-challenge-clj.tuple :refer :all]
+             [clojure.string :as str]))
+
+(defn apply-to-n-lines [f n string]
+  (str/join "\n" (f n (str/split-lines string))))
 
 (deftest tuple-test
   (testing "tuple with w=1.0 is a point"
@@ -100,15 +104,114 @@
          (vector-tuple 1 -2 1)
          (cross-product b a)))))
 
+(deftest color-test
+  (let [c (color -0.5 0.4 1.7)]
+    (is (= -0.5 (:red c)))
+    (is (= 0.4 (:green c)))
+    (is (= 1.7 (:blue c)))))
+
+(deftest hadamard-product-test
+  (let [c1 (color 1 0.2 0.4)
+        c2 (color 0.9 1 0.1)]
+    (is (tuples-equal? (color 0.9 0.2 0.04) (hadamard-product c1 c2)))))
+
+(deftest canvas-test
+  (let [c (canvas 10 20)]
+    (is (= 10 (:width c)))
+    (is (= 20 (:height c)))
+    (is (= 20 (count (:rows c))))
+    (is (= 200 (count (flatten (:rows c)))))
+    (every? (fn [p] (= (color 0 0 0) p)) (flatten (:rows c)))))
+
+(deftest write-pixel-test
+  (let [c (canvas 10 20)
+        red (color 1 0 0)
+        c' (write-pixel c 2 3 red)]
+    (is (= red (pixel-at c' 2 3)))))
+
+(defn first-n-lines [n string]
+  (apply-to-n-lines take n string))
+
+(defn last-n-lines [n string]
+  (apply-to-n-lines take-last n string))
+
+(deftest canvas-to-ppm-test
+  (testing "header"
+    (let [c (canvas 5 3)
+          ppm (canvas-to-ppm c)]
+      (is (= (multi-line-string "P3" "5 3" "255")
+             (first-n-lines 3 ppm)))))
+
+  (testing "constructing pixel data"
+    (let [c (canvas 5 3)
+          c1 (color 1 0 0)
+          c2 (color 0 0.5 0)
+          c3 (color 0 0 1)
+          result (-> c
+                     (write-pixel 0 0 c1)
+                     (write-pixel 2 1 c2)
+                     (write-pixel 4 2 c3)
+                     canvas-to-ppm)]
+      (is (= (multi-line-string
+              "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+              "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"
+              "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255")
+             (last-n-lines 3 result)))))
+
+  (testing "splitting long lines"
+    (let [c (canvas 10, 2 (color 1 0.8 0.6))
+          ppm (canvas-to-ppm c)]
+      (is (= (multi-line-string
+              "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+              "153 255 204 153 255 204 153 255 204 153 255 204 153"
+              "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"
+              "153 255 204 153 255 204 153 255 204 153 255 204 153")
+             (last-n-lines 4 ppm))))))
+
+(deftest color->str-test
+  (is (= "0 128 0" (color->str (color 0 0.5 0))))
+
+  (testing "clamping to 0"
+    (is (= "0 0 0" (color->str (color -0.5 0 0))))))
+
+(deftest write-line-test
+  (is (= "0 0 0 255 255 255"
+         (write-line [(color 0 0 0) (color 1 1 1)]))))
+
+(deftest should-include?-test
+  (let [include? (partial should-include? 5)]
+    (is (include? "123" "4"))
+    (is (include? "12" "34"))
+    (is (not (include? "1234" "5")))
+    (is (not (include? "12345" "6")))
+    (is (not (include? "123" "45")))))
+
+(deftest format-line-test
+  (let [subject (partial format-line 5)]
+    (is (= ["ab cd"] (subject ["ab" "cd"])))
+    (is (= ["abc" "de"] (subject ["abc" "de"])))
+    (is (= ["abcde" "f"] (subject ["abcde" "f"])))
+    (is (= ["ab cd" "ef"] (subject ["ab" "cd" "ef"])))))
+
+(deftest format-body-test
+  (let [subject (partial format-body 10)]
+    (testing "leaves it alone if doesn't need formatting"
+      (is (= (multi-line-string "ab cd df" "gh hi jk")
+             (subject (multi-line-string "ab cd df" "gh hi jk")))))
+    (testing "evens it out when it needs it"
+      (is (= (multi-line-string "abcdefghi" "jkl")
+             (subject "abcdefghi jkl")))
+      (is (= (multi-line-string "123 456 78" "9 10")
+             (subject "123 456 78 9 10"))))))
 
 (defn tick [env proj]
   (let [position (add-tuples (:position proj) (:velocity proj))
         velocity (add-tuples (:velocity proj) (:gravity env) (:wind env))]
     (prn (:x position) (:y position))
-    { :position position :velocity velocity }))
+    {:position position :velocity velocity}))
 
-(def p { :position (point-tuple 0 1 0) :velocity (vector-tuple 1 3 0)})
-(def e { :gravity (vector-tuple 0 -0.1 0) :wind (vector-tuple -0.01 0 0)})
+(def p {:position (point-tuple 0 1 0) :velocity (vector-tuple 1 3 0)})
+(def e {:gravity (vector-tuple 0 -0.1 0) :wind (vector-tuple -0.01 0 0)})
 
 (defn next-tick []
   (def p (tick e p)))
